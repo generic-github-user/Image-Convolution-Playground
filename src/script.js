a1a0cf26c0ba0d469583e6519bd8132957bb4ec3
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-09-30 11:38:34 -0400

    Moved kernel information into a separate JavaScript file
    Attempted to use a JSON file loaded through jQuery, but this was creation loading issues and overcomplicated data storage.

--- src/script.js
+++ src/script.js
@@ -15,77 +15,14 @@ const find_anchor = function(kernel) {
       return anchor;
 }
 
-var kernels = [{
-            "name": "Identity",
-            "kernel": [
-                  [0, 0, 0],
-                  [0, 1, 0],
-                  [0, 0, 0]
-            ]
-      },
-      {
-            "name": "Sharpen",
-            "kernel": [
-                  [0, -1, 0],
-                  [-1, 5, -1],
-                  [0, -1, 0]
-            ]
-      },
-      {
-            "name": "Box blur",
-            "factor": 1 / 9,
-            "kernel": [
-                  [1, 1, 1],
-                  [1, 1, 1],
-                  [1, 1, 1]
-            ]
-      },
-      {
-            "name": "Gaussian blur (3 by 3)",
-            "factor": 1 / 16,
-            "kernel": [
-                  [1, 2, 1],
-                  [2, 4, 2],
-                  [1, 2, 1]
-            ]
-      },
-      {
-            "name": "Gaussian blur (5 by 5)",
-            "factor": 1 / 256,
-            "kernel": [
-                  [1, 4, 6, 4, 1],
-                  [4, 16, 24, 16, 4],
-                  [6, 24, 36, 24, 6],
-                  [4, 16, 24, 16, 4],
-                  [1, 4, 6, 4, 1]
-            ]
-      },
-      {
-            "name": "Unsharp Masking",
-            "factor": -1 / 256,
-            "kernel": [
-                  [1, 4, 6, 4, 1],
-                  [4, 16, 24, 16, 4],
-                  [6, 24, -476, 24, 6],
-                  [4, 16, 24, 16, 4],
-                  [1, 4, 6, 4, 1]
-            ]
-      },
-      {
-            "name": "Custom",
-            "kernel": [
-                  [0, 0, 0],
-                  [0, 1, 0],
-                  [0, 0, 0]
-            ]
-      }
-];
-
 kernels.forEach(
       (kernel) => {
             if (!kernel.factor) {
                   kernel.factor = 1;
+            } else {
+                  kernel.factor = eval(kernel.factor);
             }
+
             if (!kernel.anchor) {
                   kernel.anchor = find_anchor(kernel);
             }
